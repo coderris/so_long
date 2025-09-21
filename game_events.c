01c457c7f2722760dc1e46a6f12bcc6248884698
Monerris <129610054+coderris@users.noreply.github.com>
2025-09-21 17:20:42 +0200

    Add files via upload

---
+++ game_events.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_events.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lanton-m <lanton-m@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 15:59:36 by lanton-m          #+#    #+#             */
+/*   Updated: 2025/09/21 13:13:43 by lanton-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+void	ft_gameplay_start(t_game_instance *game_init)
+{
+	mlx_hook(game_init->win_ptr, 17, 0, ft_exit_program, game_init);
+	mlx_hook(game_init->win_ptr, 2, (1L << 0), ft_check_keyboard, game_init);
+	mlx_loop_hook(game_init->mlx_ptr, &ft_map_draw, game_init);
+}
+
+int	ft_print_shell(t_game_instance *game_init)
+{
+	static int	previous_count_movements;
+	int			current_count_movements;
+
+	previous_count_movements = -1;
+	current_count_movements = game_init->game_data.count_movements;
+	if (current_count_movements != previous_count_movements)
+	{
+		ft_printf("You moved %d times.\n", current_count_movements);
+		previous_count_movements = current_count_movements;
+	}
+	return (1);
+}
+
+int	ft_events_pressed(t_game_instance *game_init, int column, int row)
+{
+	int	new_row;
+	int	new_col;
+	int	current_tile;
+
+	ft_locate_player(game_init);
+	new_row = game_init->positions_init.player_row + row;
+	new_col = game_init->positions_init.player_col + column;
+	current_tile = game_init->map_init.matrice[new_row][new_col];
+	if (current_tile == EMPTY || current_tile == COLLECTIBLE)
+	{
+		game_init->map_init.matrice[new_row][new_col] = PLAYER;
+		game_init->map_init.matrice[game_init->positions_init.player_row]
+		[game_init->positions_init.player_col] = EMPTY;
+		game_init->positions_init.player_row = new_row;
+		game_init->positions_init.player_col = new_col;
+		if (current_tile == COLLECTIBLE)
+			game_init->game_data.count_collectible--;
+		game_init->game_data.count_movements++;
+	}
+	else if (current_tile == EXIT
+		&& game_init->game_data.count_collectible == 0)
+		ft_win(game_init);
+	return (ft_print_shell(game_init));
+}
+
+void	ft_locate_player(t_game_instance *game_init)
+{
+	int	col;
+	int	row;
+
+	row = 0;
+	while (game_init->map_init.matrice[row] != NULL)
+	{
+		col = 0;
+		while (game_init->map_init.matrice[row][col] != '\0')
+		{
+			if (game_init->map_init.matrice[row][col] == PLAYER)
+			{
+				game_init->positions_init.player_row = row;
+				game_init->positions_init.player_col = col;
+				return ;
+			}
+			col++;
+		}
+		row++;
+	}
+}
---
+++ map_validate.c
@@ -0,0 +1,137 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_validate.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lanton-m <lanton-m@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 16:00:16 by lanton-m          #+#    #+#             */
+/*   Updated: 2025/09/21 12:51:07 by lanton-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+#include <stdlib.h>
+
+int	ft_is_valid_map(t_game_instance *game_init)
+{
+    if (!(ft_count_map_objects(game_init)))
+	    return (0);
+    if (!(ft_is_map_shape_valid(game_init)))
+	    return (0);
+    if (!(ft_is_wall(game_init)))
+	    return (0);
+    if (!(ft_is_map_resoluble(game_init)))
+		return (0);
+    return (1);
+}
+
+int	ft_count_map_objects(t_game_instance *game_init)
+{
+	int row;
+	int column;
+	char c;
+
+	if (!game_init || !game_init->map_init.matrice)
+		return (0);
+
+	game_init->game_data.count_player = 0;
+	game_init->game_data.count_exit = 0;
+	game_init->game_data.count_collectible = 0;
+	game_init->game_data.count_wall = 0;
+	game_init->game_data.count_empty = 0;
+
+	row = 0;
+	while (game_init->map_init.matrice[row])
+	{
+		column = 0;
+		while (game_init->map_init.matrice[row][column])
+		{
+			c = game_init->map_init.matrice[row][column];
+			if (c == '\n')
+				break;
+			if (c == PLAYER)
+				game_init->game_data.count_player++;
+			else if (c == EXIT)
+				game_init->game_data.count_exit++;
+			else if (c == COLLECTIBLE)
+				game_init->game_data.count_collectible++;
+			else if (c == WALL)
+				game_init->game_data.count_wall++;
+			else if (c == EMPTY)
+				game_init->game_data.count_empty++;
+			else
+				return (0);
+			column++;
+		}
+		row++;
+	}
+	return (ft_have_requires(game_init));
+}
+
+
+int	ft_is_map_shape_valid(t_game_instance *game_init)
+{
+	int i;
+	int j;
+	int row_len;
+	int ref_len;
+
+	if (!game_init || !game_init->map_init.matrice)
+		return (0);
+	if (game_init->map_init.rows_matrice <= 0)
+		return (0);
+	ref_len = -1;
+	i = 0;
+	while (i < game_init->map_init.rows_matrice)
+	{
+		row_len = 0;
+		j = 0;
+		while (game_init->map_init.matrice[i][j] && game_init->map_init.matrice[i][j] != '\n')
+		{
+			row_len++;
+			j++;
+		}
+		if (ref_len == -1)
+			ref_len = row_len;
+		else if (row_len != ref_len)
+			return (0);
+		i++;
+	}
+	game_init->map_init.cols_matrice = ref_len;
+	return (1);
+}
+
+
+
+int	ft_is_wall(t_game_instance *game_init)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+    while (i < game_init->map_init.rows_matrice)
+    {
+	    j = 0;
+	    while (j < game_init->map_init.cols_matrice)
+	    {
+		    if (i == 0 || j == 0 || i == game_init->map_init.rows_matrice - 1 || j == game_init->map_init.cols_matrice - 1)
+		    {
+			    if (game_init->map_init.matrice[i][j] != WALL)
+				    return (0);
+		    }
+		    j++;
+	    }
+	    i++;
+    }
+	return (1);
+}
+
+int	ft_have_requires(t_game_instance *game_init)
+{
+	if (!(game_init->game_data.count_player == 1
+			&& game_init->game_data.count_exit == 1
+			&& game_init->game_data.count_collectible >= 1))
+		return (0);
+	return (1);
+}
